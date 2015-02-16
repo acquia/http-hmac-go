0e7d15e204ec7ac9a75f1d87894c930f963b4ca5
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2015-02-15 22:03:54 -0500

    Added a Godoc badge and docs.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # HTTP HMAC Signer for Go
 
 [![Build Status](https://travis-ci.org/acquia/http-hmac-go.svg)](https://travis-ci.org/acquia/http-hmac-go)
+[![GoDoc](https://godoc.org/github.com/acquia/http-hmac-go?status.svg)](https://godoc.org/github.com/acquia/http-hmac-go)
 
 An implementation of the [HTTP HMAC Spec](https://github.com/acquia/http-hmac-spec)
 in [Golang](https://golang.org/).
--- message.go
+++ message.go
@@ -1,3 +1,5 @@
+// Package httphmac provides functions for signing HTTP requests, implementing
+// the HTTP HMAC Spec. https://github.com/acquia/http-hmac-spec
 package httphmac
 
 import (
@@ -13,15 +15,33 @@ import (
 	"strings"
 )
 
+// A Message represents the parts of the HTTP request used in the generation of
+// the HMAC signature.
 type Message struct {
-	Method        string
-	BodyHash      string
-	ContentType   string
-	Date          string
+	// Method specifies the HTTP method (GET, POST, PUT, etc.).
+	Method string
+
+	// BodyHash specifies the MD5 hash of the raw body of the HTTP request.
+	BodyHash string
+
+	// ContentType specifies the value of the "Content-type" header.
+	ContentType string
+
+	// Date specifies the value of the "Date" header or a custom header, e.g.
+	// "x-acquia-timestamp".
+	Date string
+
+	// CustomHeaders specifies a collection of custom headers used in
+	// generating the signature.
 	CustomHeaders *Headers
-	Resource      *url.URL
+
+	// Resource specifies the URI being requested (for server requests) or the URL
+	// to access (for client requests).
+	Resource *url.URL
 }
 
+// NewMessage returns a new Message given a HTTP request, and an array of
+// custom headers used in the HMAC signature generation.
 func NewMessage(r *http.Request, headers ...[]string) *Message {
 
 	h := NewHeaders()
@@ -41,6 +61,8 @@ func NewMessage(r *http.Request, headers ...[]string) *Message {
 	}
 }
 
+// Bytes returns a slice of the contents of the Message used in the HMAC
+// signature generation.
 func (m *Message) Bytes() []byte {
 	var b bytes.Buffer
 
@@ -63,12 +85,14 @@ func (m *Message) Bytes() []byte {
 	return b.Bytes()
 }
 
+// Sign returns the HMAC signature.
 func (m *Message) Sign(digest func() hash.Hash, secret string) string {
 	h := hmac.New(digest, []byte(secret))
 	h.Write(m.Bytes())
 	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
 
+// HashBody returns an MD5 hash of the request body, given an HTTP request.
 func HashBody(body io.ReadCloser) string {
 	b := new(bytes.Buffer)
 	b.ReadFrom(body)
@@ -79,21 +103,27 @@ func HashBody(body io.ReadCloser) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// A Headers represents the custom headers used in the HMAC signature generation.
 type Headers struct {
+	// Values specifies the custom headers used in the HMAC signature generation.
 	values map[string]string
 }
 
+// NewHeaders returns a new Headers.
 func NewHeaders() *Headers {
 	return &Headers{
 		values: make(map[string]string),
 	}
 }
 
+// Set sets the value of a custom header used in the HMAC signature generation.
 func (h *Headers) Set(header, value string) *Headers {
 	h.values[strings.ToLower(header)] = value
 	return h
 }
 
+// Bytes returns a slice of the contents of the custom headers used in the HMAC
+// signature generation.
 func (h *Headers) Bytes() []byte {
 	var b bytes.Buffer
 

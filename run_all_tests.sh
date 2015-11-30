c8177fd432aa3809ee07d3683167e8b934163605
Ákos Tóth <akos.toth@cheppers.com>
2015-11-30 19:45:43 +0100

    channel down the exit code

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -5,17 +5,27 @@ if [ -z "$cdir" ]; then
 	cdir="."
 fi
 
+ec=0
 for fn in "$cdir"/*; do
 	if [ -d "$fn" ]; then
 		$0 "$fn"
+		ed=$?
+		if [ $ec -eq 0 ]; then
+			ec=$ed
+		fi
 	fi
 done
+
 if [ -n "$(find "$cdir" -maxdepth 1 -name '*_test.go' -print -quit)" ]; then
-	echo "$cdir: $(shopt -s nullglob; echo "$cdir/"*_test.go)"
+	echo "$cdir: $(find "$cdir" -maxdepth 1 -name '*_test.go' -print -quit)"
 	cd $cdir
 	go test -v -cover
+	ed=$?
+	if [ $ec -eq 0 ]; then
+		ec=$ed
+	fi
 	cd - >/dev/null
-	exit 0
+	exit $ec
 fi
 
 exit 0
